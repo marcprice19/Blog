ceeee12ca4d9607a073576fc4b951bb70f6048d5
Marc Price <dpl@DPL-ADMISSIONS-A1398s-MacBook-Pro.local>
2019-11-19 18:05:17 -0700

    Practice with model, contollet, creating pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
+  resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
